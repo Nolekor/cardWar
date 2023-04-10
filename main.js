6ac707f2c527ff3b01ef15e8e0cacfaa8d637ed6
Nolekor <78369850+Nolekor@users.noreply.github.com>
2023-04-10 20:08:11 +0200

    add function for get a new deck and draw new cards

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
+// https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/
+const shuffle = document.querySelector("#new-deck");
+const draw = document.querySelector("#draw");
+let deckId;
 
+shuffle.addEventListener("click", handleClick);
+draw.addEventListener("click", drawCards);
+
+function handleClick() {
+  fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+    .then((res) => res.json())
+    .then((deck) => (deckId = deck.deck_id));
+  draw.disabled = false;
+}
+function drawCards() {
+  fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    .then((res) => res.json())
+    .then((cards) => {
+      console.log(cards);
+      cards.cards.map((card) => {
+        let html = `
+      <img src="${card.images.svg}">
+      `;
+        document.getElementById("app").innerHTML += html;
+      });
+    });
+}
